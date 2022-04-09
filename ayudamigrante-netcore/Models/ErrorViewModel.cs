d975569583f58c437ff5249fa5e935931b3de733
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2022-04-09 12:30:45 -0500

    Agregar archivos de proyecto.

---
+++ ayudamigrante-netcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ayudamigrante_netcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
