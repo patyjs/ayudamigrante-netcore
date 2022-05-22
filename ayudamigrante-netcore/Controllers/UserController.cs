a46f1300df24449057feaee2c40852a1916f6023
Angel Jimenez Reyes <angel.g.j.reyes@gmail.com>
2022-05-22 08:46:41 -0500

    Controller User, Page User, UI Fix

---
+++ ayudamigrante-netcore/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+using ayudamigrante_netcore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ayudamigrante_netcore.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public UserController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("/u")]
+        public IActionResult U()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
